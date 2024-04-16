5fa7cd89358c9bebf743397aa9b0b32206a6fd2c
Charbel <charbelakiki18@gmail.com>
2024-04-16 14:39:07 +0300

    did random quote generator

---
+++ random_quote/scripts.js
@@ -0,0 +1,21 @@
+let button = document.querySelector(".btn");
+let quote = document.querySelector(".quote");
+let person = document.querySelector(".author");
+
+button.addEventListener("click", function () {
+  rand_quote();
+});
+
+const quotes = [
+  { quote: "hi", person: "bye" },
+  { quote: "yo", person: "go" },
+  { quote: "sup", person: "fk" },
+];
+
+function rand_quote() {
+  let i = Math.random();
+  i = Math.floor(i * 10) % 3;
+  let q = quotes[i];
+  quote.innerText = q.quote;
+  person.innerText = q.person;
+}
